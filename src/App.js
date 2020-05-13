627af91bb5779eda87f6e3c52ebecd20ca3462ff
Ara Jo <arajodeveloper@gmail.com>
2020-05-13 12:02:48 -0700

    needwork

--- src/App.js
+++ src/App.js
@@ -2,33 +2,64 @@ import React, { Component } from 'react'
 import './App.css'
 import Square from './components/Square'
 
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 class App extends Component{
   constructor(props){
     super(props)
+
+    var stuff = ["treasure", "bomb", "tree", "tree", "tree", "tree", "tree", "tree", "tree"];
+    shuffle(stuff);
+    console.log(stuff);
+
     this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      currentPosition: "",
+      hiddenStuff: stuff
     }
   }
+
+  handleChange = (ind) => {
+    // var hiddenStuff = this.state.hiddenStuff
+    var squares = this.state.squares
+    squares[ind] = this.state.hiddenStuff[ind]
+    this.setState({squares: squares})
+  }
+
+
+
 
   render(){
     let square = this.state.squares.map((value, index) => {
       return (
-        <Square value={value} />
+        <Square value={value} index={index} key={index} click={this.handleChange}/>
       )
     })
     return(
       <React.Fragment>
-        <div>
         <h1>Treasure Hunt App</h1>
-        <div id="square1">
-        { square.slice(0,3) }
-        <div id="square2">
-        { square.slice(3,6) }
-        </div>
-        <div id="square3">
-        { square.slice(6,9) }
-        </div>
-        </div>
+
+        <div id="game-board">
+          {square}
+
         </div>
       </React.Fragment>
     )
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+  constructor(props){
+    super(props)
+  }
   render(){
     return(
       <React.Fragment>
-      <div id="square">
+      <div id="square" onClick={this.props.click(this.props.index)}>
       {this.props.value}
       </div>
 
