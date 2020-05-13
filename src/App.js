1a0fe9d9c0e3f9f25b9fbdaf005820984712e43f
Ara Jo <arajodeveloper@gmail.com>
2020-05-13 10:26:11 -0700

    need to fix box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component{
   constructor(props){
@@ -10,9 +11,25 @@ class App extends Component{
   }
 
   render(){
+    let square = this.state.squares.map((value, index) => {
+      return (
+        <Square value={value} />
+      )
+    })
     return(
       <React.Fragment>
+        <div>
         <h1>Treasure Hunt App</h1>
+        <div id="square1">
+        { square.slice(0,3) }
+        <div id="square2">
+        { square.slice(3,6) }
+        </div>
+        <div id="square3">
+        { square.slice(6,9) }
+        </div>
+        </div>
+        </div>
       </React.Fragment>
     )
   }
